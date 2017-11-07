98633515be85e1b7fb79ccf0a47c50e19eaa6f58
alexcambose <alexcambose@yahoo.com>
2017-11-07 21:37:32 +0200

    Initial commit

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="is-size-1 has-text-centered">Welcome home, {{user.first_name}}!</h1>
+    </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    computed: mapGetters(['user']),
+  };
+</script>
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const root = 'http://localhost:3000/api/';
+export default {
+  API: {
+    LOGIN: root + 'user/login',
+    REGISTER: root + 'user/register',
+  }
+};
